9f125f0aab86be6bedead433d04988bf3a75c4a1
GuadalupeFrias <trestrebolesamarillos@gmail.com>
2022-08-29 21:26:08 -0300

    incorporacion de mock de tareas

---
+++ task-list/src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[]=[
+{
+    id:1,
+    text:"Terminar nuestro primer modulo de Angular",
+    day:'Septiembre 5 a las 12:00',
+    reminder:true
+},
+{
+    id:2,
+    text:"Ver a River Plate",
+    day:'Septiembre 4 a las 20:00',
+    reminder:true
+},
+{
+    id:3,
+    text:"Utilzar un date time input",
+    day:'Septiembre 6 a las 20:00',
+    reminder:false
+},
+{
+    id:4,
+    text:"Marcado de tareas proximas a vencer o vencidas",
+    day:'Septiembre 7 a las 12:00',
+    reminder:false
+},
+]
