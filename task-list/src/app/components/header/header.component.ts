444178bc7516ee235bd797992f5988b49eb38d16
GuadalupeFrias <trestrebolesamarillos@gmail.com>
2022-08-31 20:47:30 -0300

    footer

--- task-list/src/app/components/header/header.component.ts
+++ task-list/src/app/components/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { UiServiceService } from 'src/app/service/ui.service.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
   title:string = 'My Task List';
   showAddTask :boolean =true;
   subscription?: Subscription;
-  constructor( private uiServiceService:UiServiceService) {
+
+  constructor( 
+    private uiServiceService:UiServiceService,
+    private router:Router
+    )  {
    this.subscription = this.uiServiceService.onToggle()
                         .subscribe(value => this.showAddTask=value)
    }
@@ -19,5 +25,8 @@ export class HeaderComponent implements OnInit {
   }
   toggleAddTask(){
   this.uiServiceService.toogleaAddTask();
+  }
+  hasRoute(route:string){
+    return this.router.url ===route
   }
 }
